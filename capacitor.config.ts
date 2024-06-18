6a04b53600e1db1ac916eb77112afc32a378b5f1
EshanDeb <41776550+EshanDeb@users.noreply.github.com>
2024-06-18 17:48:01 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ionicMiniApp1',
-  webDir: 'dist'
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '0b3b44b6',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
